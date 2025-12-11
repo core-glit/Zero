a36b8d8533cf494843ceaff72ea1285f72feb180
DHANUSH <gdhanush1723@gmail.com>
2025-12-12 02:12:51 +0530

    feat: Add new portfolio website with styling, a multi-page project with images, and a Java BubbleSort implementation.

--- Projects/java/BubbleSort.java
+++ Projects/java/BubbleSort.java
@@ -1,8 +1,28 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        bubbleSort(arr);
-        System.out.println("Sorted array:");
-        printArray(arr);
-    }
+        //input [3,3,5,5,7]
+        //output 15 with bubble sort
+        //remove duplicates and sum
+        int[] arr = {3,3,5,5,7};
+        
+        int n = arr.length; 
+        for (int i = 0; i < n - 1; i++) {  
+            for (int j = 0; j < n - i - 1; j++) { 
+                if (arr[j] > arr[j + 1]) {  
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0 || arr[i] != arr[i - 1]) {
+                sum += arr[i];
+            }
+        }
+        
+        System.out.println(sum); 
+    }       
 }
